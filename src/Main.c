5cf9a3bc30107012cbc4dc93c1cd82f43d8c33e0
C-Reaper <gnu.c.reaper@gmail.com>
2025-11-06 18:19:15 +0100

    update

--- src/Main.c
+++ src/Main.c
@@ -4,10 +4,10 @@ int main() {
     
     TerminalCtl tc = TerminalCtl_New();
 
-    TerminalCtl_Resize(&tc,(Coord){ 1,1 });
+    TerminalCtl_Resize(&tc,(Term_Coord){ 1,1 });
     TerminalCtl_Update(&tc);
     TerminalCtl_Clear(&tc);
-    TerminalCtl_SetColor(&tc,Color_New(0,0,0,0,0,0));
+    TerminalCtl_SetTerm_Color(&tc,Term_Color_New(0,0,0,0,0,0));
     TerminalCtl_Reset(&tc);
 
     TerminalCtl_Free(&tc);
